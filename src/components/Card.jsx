879d75e7ab5ed051c10b08e5f658a4d6cfa48066
Priyankaadev <priyaaswal79@gmail.com>
2024-09-12 22:21:38 +0530

    added Cards functionality

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,7 +4,7 @@ import { IoClose } from "react-icons/io5";
 
 function Card({ data }) {
     return (
-        <div className=" relative w-60 h-72 px-8 py-10 rounded-[45px] overflow-hidden bg-zinc-900/90 text-white">
+        <div className=" relative flex-shrink-0 w-60 h-72 px-8 py-10 rounded-[45px] overflow-hidden bg-zinc-900/90 text-white">
             <FaRegFileAlt />
             <p className="text-sm mt-5 font-semibold leading-tight">{data.desc}.</p>
             <div className="footer absolute bottom-0 w-full left-0">
@@ -18,10 +18,10 @@ function Card({ data }) {
                     </span>
                 </div>
                 {
-                    data.tag.isOpen ?(
-                    <div className="tag w-full py-4 bg-green-600 flex items-center justify-center">
-                        <h3 className="text-sm font-semibold "> Download Now</h3>
-                    </div> )  : null
+                    data.tag.isOpen && (
+                    <div className={`tag w-full py-4 ${data.tag.tagColor === "blue" ? "bg-blue-600" : "bg-green-600"} flex items-center justify-center`}>
+                        <h3 className="text-sm font-semibold "> {data.tag.tagTitle}</h3>
+                    </div> ) 
                 }
 
             </div>
--- src/components/Foreground.jsx
+++ src/components/Foreground.jsx
@@ -9,13 +9,25 @@ function Foreground() {
             close: true,
             tag: { isOpen: true, tagTitle: "Download Now", tagColor: 'green' },
         },
+        {
+            desc: 'This is the background color of the card that will be displayed',
+            filesize: '0.4mb',
+            close: true,
+            tag: { isOpen: true, tagTitle: "Download Now", tagColor: 'blue' },
+        },
+        {
+            desc: 'This is the background color of the card that will be displayed',
+            filesize: '0.4mb',
+            close: true,
+            tag: { isOpen: false, tagTitle: "Upload", tagColor: 'green' },
+        },
     ]
 
 
 
 
     return (
-        <div className="fixed top-0 left-0 z-[3] w-full h-full">
+        <div className="fixed top-0 left-0 z-[3] w-full h-full flex gap-5 flex-wrap p-5">
            {data.map((item, index)=>(
             <Card data={item}/>
            ))}
